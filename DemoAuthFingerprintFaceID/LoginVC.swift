4da8332b7c42767fca2fd0990879c4313eabe0c5
AkshaySharma <akshay.as597@gmail.com>
2024-05-26 18:39:56 +0530

    Add Sucess View controllers and login vc

---
+++ DemoAuthFingerprintFaceID/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  DemoAuthFingerprintFaceID
+//
+//  Created by Akshay Kumar on 26/05/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    //MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- DemoAuthFingerprintFaceID/SucessVC.swift
+++ DemoAuthFingerprintFaceID/SucessVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SucessVC.swift
 //  DemoAuthFingerprintFaceID
 //
 //  Created by Akshay Kumar on 26/05/24.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SucessVC: UIViewController {
 
+    //MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
